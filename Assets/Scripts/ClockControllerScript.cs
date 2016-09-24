d31f6649bc2809aa0d531aa8c1dc15d388bcfc20
Austin Ferguson <ajartcga@gmail.com>
2016-09-23 23:39:06 -0400

    First real update

---
+++ Assets/Scripts/ClockControllerScript.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClockControllerScript : MonoBehaviour {
+
+    GameObject hourHand, minuteHand, secondHand;
+    PlayerScript player1, player2, player3, player4;
+    public float minuteSpeed = 180;
+    public float hourSpeed = 3;
+	// Use this for initialization
+	void Start ()
+    {
+        hourHand = GameObject.Find("HourHandOrigin");
+        minuteHand = GameObject.Find("MinuteHandOrigin");
+        player1 = GameObject.Find("Player1").GetComponent<PlayerScript>();
+        player2 = GameObject.Find("Player2").GetComponent<PlayerScript>();
+        player3 = GameObject.Find("Player3").GetComponent<PlayerScript>();
+        player4 = GameObject.Find("Player4").GetComponent<PlayerScript>();
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float dt = Time.deltaTime;
+        float pi = Mathf.PI;
+        minuteHand.transform.Rotate(new Vector3(0, dt * minuteSpeed, 0));
+        hourHand.transform.Rotate(new Vector3(0, dt * hourSpeed, 0));
+        if (minuteHand.transform.rotation.y > 360)
+        {
+            minuteHand.transform.Rotate(0, -360, 0);
+        }
+        checkMin();
+        checkHour();
+    }
+    private void checkMin()
+    {
+        float min = minuteHand.transform.rotation.eulerAngles.y;
+
+        if (min > 0 && min < 90)
+        {
+            player1.setMin();
+            player4.removeMin();
+        }
+        else if (min > 90 && min < 180)
+        {
+            player2.setMin();
+            player1.removeMin();
+        }
+        else if (min > 180 && min < 270)
+        {
+            player3.setMin();
+            player2.removeMin();
+        }
+        else if (min > 270 && min < 360)
+        {
+            player4.setMin();
+            player3.removeMin();
+        }
+    }
+    private void checkHour()
+    {
+        float hour = hourHand.transform.rotation.eulerAngles.y;
+
+        if (hour > 0 && hour < 90)
+        {
+            player1.setHour();
+            player4.removeHour();
+        }
+        else if (hour > 90 && hour < 180)
+        {
+            player2.setHour();
+            player1.removeHour();
+        }
+        else if (hour > 180 && hour < 270)
+        {
+            player3.setHour();
+            player2.removeHour();
+        }
+        else if (hour > 270 && hour < 360)
+        {
+            player4.setHour();
+            player3.removeHour();
+        }
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,26 +3,31 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 
-    public int mControllerNum = 0;
+    public int mControllerNum = 1;
     public float speed = 0.3f;
+
+    bool mIsMin;
+    bool mIsHour;
+    Color mStartColor;
 
     float x, z;//These are here temporarily, don't forget to move them
 
 	// Use this for initialization
 	void Start ()
-    { 
-
+    {
+        mStartColor = GetComponent<MeshRenderer>().material.color;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        x = Input.GetAxis("Horizontal");
-        z = Input.GetAxis("Vertical");
+        x = Input.GetAxis("Player" + mControllerNum + "X");
+        z = Input.GetAxis("Player" + mControllerNum + "Y");
         //transform.Translate(new Vector3(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed));
-        transform.position = transform.position + new Vector3(x * speed, 0, z * speed);
+       
         if((x > 0.1f || x < -0.1f) || (z > 0.1f || z < -0.1f))
             transform.rotation = Quaternion.Euler(0, Mathf.Atan2(-z, x) * 180 / Mathf.PI,0);
+            transform.position = transform.position + new Vector3(x * speed, 0, z * speed);
 
     }
 
@@ -30,5 +35,53 @@ public class PlayerScript : MonoBehaviour {
     {
         GUI.Label(new Rect(0, 0, 100, 20), "X: " + x);
         GUI.Label(new Rect(0, 20, 100, 20), "Z: " + z);
+    }
+
+    public void setMin()
+    {
+        if (!mIsMin)
+        {
+            mIsMin = true;
+            if (!mIsHour)
+            {
+                GetComponent<MeshRenderer>().material.color = Color.cyan;
+            }
+        }
+    }
+    public void removeMin()
+    {
+        if (mIsMin)
+        {
+            mIsMin = false;
+            GetComponent<MeshRenderer>().material.color = mStartColor;
+        } 
+    }
+
+    public void setHour()
+    {
+        if (!mIsMin)
+        {
+            mIsMin = true;
+            if (!mIsHour)
+            {
+                GetComponent<MeshRenderer>().material.color = Color.black;
+            }
+        }
+    }
+    public void removeHour()
+    {
+        if (mIsMin)
+        {
+            mIsMin = false;
+            if (mIsMin)
+            {
+                GetComponent<MeshRenderer>().material.color = Color.cyan;
+            }
+            else
+            {
+                GetComponent<MeshRenderer>().material.color = mStartColor;
+            }
+            
+        }
     }
 }
