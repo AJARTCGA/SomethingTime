55280cb551624023d35cadae7b8dd416d465e61c
Austin Ferguson <ajartcga@gmail.com>
2016-09-24 23:13:16 -0400

    Menu added

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+    }
+    public void menu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/RotateClock.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateClock : MonoBehaviour {
+    GameObject hourHand, minuteHand;
+    public float minuteSpeed = 180;
+    public float hourSpeed = 3;
+    // Use this for initialization
+    void Start () {
+        hourHand = GameObject.Find("HourHandOrigin");
+        minuteHand = GameObject.Find("MinuteHandOrigin");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float dt = Time.deltaTime;
+        minuteHand.transform.Rotate(new Vector3(0, 0, -dt * minuteSpeed));
+        hourHand.transform.Rotate(new Vector3(-dt * hourSpeed, 0, 0));
+
+        float hour = (hourHand.transform.rotation.eulerAngles.y - 180) * Mathf.Deg2Rad;
+    }
+}
